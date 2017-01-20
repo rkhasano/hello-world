67316233db6a8050b623aa6981ad0fd762986e52
rkhasano <rkhasano@online.jccc.edu>
2017-01-19 20:04:11 -0600

    Add files via upload

---
+++ labs/Source2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	float tspBakingSoda = 1*ratio;
+	float tspBakingPowder = 0.5*ratio;
+	float cupsofButter = 1*ratio;
+	float cupsofSugar = 1.5*ratio;
+	float eggs = 1*ratio;
+	float ratio;
+
+	cout << "How many batches do you want to make?";
+	cin >> ratio;
+
+	cout << tspBakingSoda <<" tsp baking soda" << endl;
+	cout << tspBakingPowder <<" tsp baking powder" << endl;
+	cout << cupsofButter" c butter" << endl;
+	cout << cupsofSugar" c white Sugar" << endl;
+	cout << eggs <<" egg" << endl;
+
+
+	while (true);
+	return 0;
+}
